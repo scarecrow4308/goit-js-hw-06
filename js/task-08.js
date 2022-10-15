30f17c0ef62d294d0b9dd4be9b90747a9f1eaf7f
scarecrow <trender.man21@gmail.com>
2022-10-15 15:08:11 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -2,12 +2,6 @@
 
 const formEl = document.querySelector('.login-form');
 
-const firstLabelEl = formEl.firstElementChild;
-const firstInputEl = firstLabelEl.lastElementChild;
-
-const secondLabelEl = firstLabelEl.nextElementSibling;
-const secondInputEl = secondLabelEl.lastElementChild;
-
 const formBtnEL = document.querySelector('button');
 
 class UserType {
@@ -17,19 +11,16 @@ class UserType {
   }
 }
 
-const arrOfUsers = [];
-
 formEl.addEventListener('submit', event => {
   event.preventDefault();
-  if (firstInputEl.value === '' || secondInputEl.value === '') {
+  const { email, password } = event.target.elements;
+  if (email.value === '' || password.value === '') {
     return alert('Fill in the gaps!');
   }
 
-  const { email, password } = event.target.elements;
   const user = new UserType(email.value, password.value);
 
-  arrOfUsers.push(user);
-  console.log(arrOfUsers);
+  console.log(user);
 
   formEl.reset();
 });
